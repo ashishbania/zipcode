fff35dee3d8358b1ccb9c3ed5dfb31e6c8ad13a6
ashishbania <60585651+ashishbania@users.noreply.github.com>
2023-06-17 19:16:31 -0400

    service and controller test added

--- src/test/java/com/example/zipcode/ControllerTest.java
+++ src/test/java/com/example/zipcode/ControllerTest.java
@@ -1,11 +1,30 @@
 package com.example.zipcode;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
 class ControllerTest {
 
+  @InjectMocks Controller controller;
+  @Mock Service service;
+
+  @BeforeEach
+  void setUp() {
+    this.controller = new Controller(this.service);
+  }
+
   @Test
-  void getZipcode() {}
+  void testGetZipcode() {
+    when(service.getZipcodeData(123)).thenReturn(new LocationDTO());
+    controller.getZipcode(123);
+    verify(service, times(1)).getZipcodeData(123);
+  }
 }
